cb94b52fda77c23e5614ce16129701f8aa935c5e
Paweł Brzozowski <pawel@brzoza.net>
2023-03-28 14:03:44 +0200

    Basis of search functionality

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+const dataEl = document.getElementById("data");
+const inputEl = document.getElementById("input");
+const searchButtonEl = document.getElementById("search");
+
+
+function getWeatherData(city) {
+  fetch(
+    `http://api.weatherapi.com/v1/current.json?key=bf3ec35ab33a49ca9cb140734232703&q=${city}&aqi=no`
+  )
+    .then(response => response.json())
+    // .then((data) => console.log(data));
+    .then(data => {
+      dataEl.textContent = JSON.stringify(data, null, 4);
+    });
+};
+
+
+searchButtonEl.addEventListener("click", event => {
+    event.preventDefault();
+    getWeatherData(inputEl.value);
+});
