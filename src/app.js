77a617f53cb1ce9ef180197380e157dacca4da98
Paweł Brzozowski <pawel@brzoza.net>
2023-03-28 16:05:22 +0200

    Basic functionality

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const dataEl = document.getElementById("data");
 const inputEl = document.getElementById("input");
 const searchButtonEl = document.getElementById("search");
-const messageEl = document.getElementById("message");
+const ErrorMessageEl = document.getElementById("error-message");
+
+const weatherConditionEl = document.getElementById("weather-condition");
 
 
 function getWeatherData(city) {
     
     dataEl.textContent = "";
-    messageEl.textContent = "";  
+    ErrorMessageEl.textContent = "";  
+    weatherConditionEl.textContent = "";
     
     fetch(`http://api.weatherapi.com/v1/current.json?key=bf3ec35ab33a49ca9cb140734232703&q=${city}&aqi=no`)
     .then(response => response.json())
@@ -15,14 +18,17 @@ function getWeatherData(city) {
     .then(data => {
       
       if (data.error) {
-        messageEl.textContent = data.error.message;
+        ErrorMessageEl.textContent = data.error.message;
       } else {
+        weatherConditionEl.textContent = `Weather condition: ${data.current.condition.text}`;
         dataEl.textContent = JSON.stringify(data, null, 4);
       };
 
+    })
+    .catch(() => {
+        console.log("Data download error!");
     });
 };
-
 
 searchButtonEl.addEventListener("click", event => {
     event.preventDefault();
